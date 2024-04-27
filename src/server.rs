cc93591c8982581020061a2c820628f321287ceb
obaraelijah <elijahobara357@gmail.com>
2024-04-27 19:33:43 +0300

    feat: Configurations

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,26 @@
 mod packets;
+use clap::Parser;
+
+#[derive(Parser,Debug)]
+struct Config {
+    port: u16,
+    domains: Vec<String>,
+}
+
 
 fn main() {
-    println!("Hello, world!");
+    tracing_subscriber::fmt::init();
+
+    let config = Config::parse();
+
+    if config.domains.is_empty() {
+        panic!("DOmains is expected");
+    }
+
+    let mut port = config.port;
+}
+
+
+fn handle_connection() -> std::io::Result<()> {
+    unimplemented!()
 }
