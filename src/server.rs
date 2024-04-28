df9af0b38ef2568bce48ed36907079c6e35735df
obaraelijah <elijahobara357@gmail.com>
2024-04-28 19:26:05 +0300

    feat: Buff parser

--- src/server.rs
+++ src/server.rs
@@ -1,15 +1,21 @@
-mod packets;
+mod packet;
 use bytes::BytesMut;
 use clap::Parser;
+use tokio::net::TcpStream;
+use tokio::io::AsyncReadExt;
 
-#[derive(Parser,Debug)]
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
 struct Config {
+    #[clap(short, long, value_parser, default_value_t = 3001)]
     port: u16,
+    #[clap(short, long, value_parser)]
     domains: Vec<String>,
 }
 
 
-fn main() -> std::io::Result<()>{
+#[tokio::main]
+async fn main() -> std::io::Result<()>{
     tracing_subscriber::fmt::init();
 
     let config = Config::parse();
@@ -25,7 +31,19 @@ fn main() -> std::io::Result<()>{
 }
 
 
-fn handle_connection() -> std::io::Result<()> {
+fn handle_connection(
+    mut conn: TcpStream
+) -> std::io::Result<()> {
     let mut buffer = BytesMut::with_capacity(4096);
+    let bytes_len = conn.read_buf(&mut buffer);
+    let packet = packet::Packet::parse(&buffer);
+    match packet {
+        packet::Packet::Init => {
+            unimplemented!()
+        }
+        packet::Packet::DataInit(domain) => {
+            unimplemented!()
+        }
+    }
     Ok(())
 }
