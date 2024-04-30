fdf36f0f06d0892c8326d932717b0c3a9f93234a
obaraelijah <elijahobara357@gmail.com>
2024-04-30 09:36:49 +0300

    feat:  Logging of HTTP request information in Logger<T> for bidirectional I/O

--- src/client.rs
+++ src/client.rs
@@ -1,10 +1,13 @@
+use ansi_term::Colour;
 use bytes::{Buf, BytesMut};
 use clap::Parser;
 use std::error::Error;
 use std::pin::Pin;
+use std::str;
 use std::sync::{Arc, Mutex};
+use std::task::{Context, Poll};
 use std::time::{Duration, Instant};
-use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt as _, ReadBuf};
 use tokio::net::TcpStream;
 
 mod packet;
@@ -67,7 +70,6 @@ async fn init(config: &Config) -> Result<String, Box<dyn Error + Send + Sync>> {
             if let Err(err) = res {
                 tracing::error!("control channel is closed by remote peer {}", err);
                 break;
-                let mut logger_dest = unimplemented!();
             }
 
             tokio::time::sleep(Duration::from_millis(500)).await;
@@ -129,8 +131,108 @@ impl LoggerState {
     }
 }
 
+/// "implement the AsyncRead trait for the Logger struct where T is bound to AsyncRead"
 impl<T: AsyncRead + AsyncWrite> AsyncRead for Logger<T> {
-    unimplemented!();
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>, // hint: that this method is supposed to be ran inside of a task buf: &mut ReadBuf<'_>,
+        buf: &mut ReadBuf<'_>, // data stored
+    ) -> Poll<std::io::Result<()>> {
+        let poll_result = self.inner.as_mut().poll_read(cx, buf);
+        if poll_result.is_ready() {
+            // do not log if the buffer is empty
+            if buf.capacity() != buf.remaining() {
+                // TODO: I think we can further optimized this by just
+                // checking the first few bytes before constructing a str
+                // from the buffer.
+                //
+                // Since, we are only interested in the first line of a HTTP payload,
+                // we can just check if it's start with GET/POST/etc, HTTP/1.1 or HTTP/2
+                if let Ok(raw_http) = str::from_utf8(buf.filled()) {
+                    // NOTE: We are only interested in the very first line of the
+                    // HTTP payload, which give us the HTTP method, path and status
+                    // code.
+                    //
+                    // So, instead of collecting all of the splitted string
+                    // into Vec, we just lazily iterate to the next one instead.
+                    // In theory, this should reduce the memory usage since we
+                    // are only consuming one line.
+                    if let Some(line) = raw_http.split('\n').next() {
+                        if !line.contains("HTTP/1.1") {
+                            return poll_result;
+                        };
+
+                        let log = line.replace("HTTP/1.1", "");
+                        let log = log.trim();
+
+                        let mut state = self.state.lock().unwrap();
+                        if let Some(instant) = state.timestamp.take() {
+                            let Some((status_code, _status)) = log.split_once(' ') else {
+                                return poll_result;
+                            };
+
+                            let Ok(status_code) = status_code.parse() else  {
+                                return poll_result;
+                            };
+
+                            let color_status = match status_code {
+                                404 => Colour::Yellow.paint(log).to_string(),
+                                status_code if status_code >= 400 => {
+                                    Colour::Red.paint(log).to_string()
+                                }
+                                status_code if status_code >= 200 => {
+                                    Colour::Green.paint(log).to_string()
+                                }
+                                _ => status_code.to_string(),
+                            };
+
+                            println!("{:<#15?} {color_status}", instant.elapsed());
+                        } else {
+                            print!("{:<20} ", log.trim());
+                            state.timestamp = Some(Instant::now());
+                        }
+                    }
+
+                    // let chunks: Vec<&str> = raw_http.split('\n').collect();
+                    // if !chunks.is_empty() && chunks[0].contains("HTTP/1.1") {
+                    //     let log = chunks[0].replace("HTTP/1.1", "");
+                    //     let log = log.trim();
+                    //
+                    //     let mut state = self.state.lock().unwrap();
+                    //     if let Some(instant) = state.timestamp.take() {
+                    //         let Some((status_code, _status)) = log.split_once(' ') else {
+                    //             return poll_result;
+                    //         };
+                    //
+                    //         let Ok(status_code) = status_code.parse() else  {
+                    //             return poll_result;
+                    //         };
+                    //
+                    //         let color_status = match status_code {
+                    //             404 => Colour::Yellow.paint(log).to_string(),
+                    //             status_code if status_code >= 400 => {
+                    //                 Colour::Red.paint(log).to_string()
+                    //             }
+                    //             status_code if status_code >= 200 => {
+                    //                 Colour::Green.paint(log).to_string()
+                    //             }
+                    //             _ => status_code.to_string(),
+                    //         };
+                    //
+                    //         println!("{:<#15?} {color_status}", instant.elapsed());
+                    //     } else {
+                    //         print!("{:<20} ", log.trim());
+                    //         state.timestamp = Some(Instant::now());
+                    //     }
+                    //
+                    // Unlock explicitly
+                    // drop(state);
+                    // }
+                }
+            }
+        }
+        poll_result
+    }
 }
 
 impl<T: AsyncWrite + AsyncRead> AsyncWrite for Logger<T> {
